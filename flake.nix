f92ddc4975bfbee8e02c1616cbbbaa1e7c098601
Vincent Haupert <mail@vincent-haupert.de>
2021-11-04 14:05:44 +0100

    Flake: test for NixOS modules

--- flake.nix
+++ flake.nix
@@ -4,9 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    # QEMU 6.1.0 hangs when running NixOS tests on a virtualized host, e.g., a GitHub Action runner
+    # Therefore, we use a NixOS Python test which still relies on QEMU 6.0.0.
+    # Upstream issue: https://github.com/NixOS/nixpkgs/issues/141596
+    nixpkgs-nixos-test.url = "github:nixos/nixpkgs/e1fc1a80a071c90ab65fb6eafae5520579163783";
+    # Only required for NixOS tests
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-nixos-test, flake-utils, home-manager }:
     let
       name = "vscode-server";
       lib = import (nixpkgs + "/lib");
@@ -41,11 +50,77 @@
         })
       )
       #
-      # CHECKS SPECIFIC TO LINUX SYSTEMS
+      # CHECKS SPECIFIC TO TARGET SYSTEMS
       #
-      (eachTargetSystem (system: {
-        # TODO: Add a test for the module
-      })
+      (eachTargetSystem (system:
+        let
+          overlayQemu6_0_0 = final: prev: {
+            qemu = (import nixpkgs-nixos-test { inherit (prev) system; }).qemu;
+          };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ overlayQemu6_0_0 ];
+          };
+          pythonTest = import (nixpkgs + "/nixos/lib/testing-python.nix") { inherit system pkgs; };
+
+          systemdUnitPathSystem = "/etc/systemd/user/auto-fix-vscode-server.service";
+          systemdUnitPathHome = "~/.config/systemd/user/auto-fix-vscode-server.service";
+        in
+        {
+          # Test for the NixOS module
+          checks."${name}-nixos" = pythonTest.makeTest {
+            name = "${name}-nixos";
+
+            machine = {
+              imports = [
+                self.nixosModules."${name}-nixos"
+              ];
+
+              services.vscode-server.enable = true;
+            };
+
+            testScript = ''
+              machine.start()
+              machine.wait_for_unit("default.target")
+
+              with subtest("Installs the system-wide systemd user service"):
+                machine.succeed("test -f ${systemdUnitPathSystem}")
+
+              with subtest("Does not install the systemd user service in home"):
+                machine.fail("test -f ${systemdUnitPathHome}")
+            '';
+          };
+
+          # Test for the Home Manager module
+          checks."${name}-home" = pythonTest.makeTest {
+            name = "${name}-home";
+
+            machine.imports = [
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.sharedModules = [
+                  self.nixosModules."${name}-home"
+                ];
+                home-manager.users.root = { ... }: {
+                  services.vscode-server.enable = true;
+                };
+              }
+            ];
+
+            testScript = ''
+              machine.start()
+              machine.wait_for_unit("multi-user.target")
+
+              with subtest("Installs the systemd user service in home"):
+                machine.succeed("test -f ${systemdUnitPathHome}")
+
+              with subtest("Does not install the system-wide systemd user service"):
+                machine.fail("test -f ${systemdUnitPathSystem}")
+            '';
+          };
+        })
       )
       #
       # SYSTEM-INDEPENDENT OUTPUTS
